9d751b4b5424d938283c50942c7dd0b4e8f338a9
iTKonnects-connectwithtechnology <srihari.maddineni.no1@gmail.com>
2025-06-16 21:33:37 +0530

    config changed, github added

--- next.config.ts
+++ next.config.ts
@@ -4,11 +4,23 @@ import type { NextConfig } from 'next';
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
   images: {
-    domains: ['printeriorstudios.com','github.com'],
+    domains: ['printeriorstudios.com', 'github.com', 'githubusercontent.com', 'raw.githubusercontent.com'],
     remotePatterns: [
       {
         protocol: 'https',
         hostname: 'printeriorstudios.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'github.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'githubusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
       },
     ],
   },
